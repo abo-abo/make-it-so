0f51fd046906fa97afca8d4a222bd5499de4cfcc
Oleh Krehel <ohwoeowho@gmail.com>
2014-03-22 16:19:06 +0100

    make-it-so.el: update interface
    * make-it-so.el (mis-source-files): New variable.
      (mis-target-files): New variable.
      (mis-make-it-so): Sets `mis-source-files', checks if they all have the
      same extension.
      (mis-abort): Simplify logic.
      (mis-finalize): Use "provide" file instead of "make provide".
      (mis-directory-files): Update doc.
      (mis-all-equal): New function.
      (mis-action): Improve.
      (mis-delete-file): New function.
      (mis-rename-quote): New function.

--- make-it-so.el
+++ make-it-so.el
@@ -90,6 +90,14 @@
 (defvar mis-mode-map
   (make-sparse-keymap))
 
+(defvar mis-source-files nil
+  "Current set of files to transform.")
+
+(defvar mis-target-files nil
+  "Mapping of `mis-source-files' to the transformation directory.
+So to cancel the transformation, rename each file in
+`mis-target-files' to `mis-source-files'.")
+
 ;; ——— Interactive —————————————————————————————————————————————————————————————
 ;;;###autoload
 (define-minor-mode mis-mode
@@ -131,15 +139,17 @@ Jump to the Makefile of the selected recipe."
 (defun mis-make-it-so ()
   "When called from `dired', offer a list of transformations."
   (interactive)
-  (let* ((target (dired-get-filename nil t))
-         (ext (file-name-extension target))
-         (candidates (mis-recipes-by-ext ext)))
-    (if candidates
-        (helm :sources
-              `((name . "Tools")
-                (candidates . ,candidates)
-                (action . mis-action)))
-      (error "No candidates for *.%s" ext))))
+  (setq mis-source-files (dired-get-marked-files nil current-prefix-arg))
+  (if (mis-all-equal (mapcar #'file-name-extension mis-source-files))
+      (let* ((ext (file-name-extension (car mis-source-files)))
+             (candidates (mis-recipes-by-ext ext)))
+        (if candidates
+            (helm :sources
+                  `((name . "Tools")
+                    (candidates . ,candidates)
+                    (action . mis-action)))
+          (error "No candidates for *.%s" ext)))
+    (error "Mixed extensions in selection")))
 
 (defun mis-file-names ()
   (let* ((dir default-directory)
@@ -155,21 +165,14 @@ Jump to the Makefile of the selected recipe."
 ;;;###autoload
 (defun mis-abort ()
   "Abort tranformation.
-This function should restore the directory to the state before
-`mis-make-it-so' was called."
+This function should revert to the state before `mis-make-it-so' was called."
   (interactive)
   (unless (file-exists-p "Makefile")
     (error "No Makefile in current directory"))
   (let ((makefile-buffer (find-buffer-visiting (expand-file-name "Makefile"))))
     (when makefile-buffer
       (kill-buffer makefile-buffer)))
-  (cl-destructuring-bind (old-file new-file)
-      (mis-file-names)
-    (rename-file old-file new-file))
-  (let ((requires (mis-slurp "requires")))
-    (mapc
-     (lambda (f) (when (file-exists-p f) (rename-file f "..")))
-     (split-string requires "\n" t)))
+  (cl-mapcar 'rename-file mis-target-files mis-source-files)
   (let ((dir default-directory))
     (dired "..")
     (delete-directory dir t))
@@ -178,34 +181,34 @@ This function should restore the directory to the state before
 ;;;###autoload
 (defun mis-finalize ()
   "Finalize transformation.
-In addition to `mis-abort' copy over the files from \"make provide\"."
+In addition to `mis-abort' copy over the files listed in \"provide\"."
   (interactive)
   (unless (file-exists-p "Makefile")
     (error "No Makefile in current directory"))
-  (let ((provides (shell-command-to-string "make provide")))
-    (if (string-match "make:" provides)
-        (error "Makefile must have a \"provide\" target")
-      (setq provides (split-string provides "\n" t))
-      (unless
-          (and (= 1 (length provides))
-               (let* ((f1 (car provides))
-                      (ext1 (file-name-extension f1))
-                      (f2 (cadr (mis-file-names)))
-                      (ext2 (file-name-extension f2)))
-                 (unless (string= ext1 ext2)
-                   (rename-file
-                    f1
-                    (format "../%s.%s"
-                            (file-name-sans-extension
-                             (file-name-nondirectory f2))
-                            ext1))
-                   t)))
-        (mapc (lambda (f) (rename-file f "..")) provides))
-      (mis-abort))))
+  (unless (file-exists-p "provide")
+    (error "No provide in current directory"))
+  (let ((provides (mis-slurp "provide")))
+    (setq provides (split-string provides "\n" t))
+    (unless
+        (and (= 1 (length provides))
+             (let* ((f1 (car provides))
+                    (ext1 (file-name-extension f1))
+                    (f2 (cadr (mis-file-names)))
+                    (ext2 (file-name-extension f2)))
+               (unless (string= ext1 ext2)
+                 (rename-file
+                  f1
+                  (format "../%s.%s"
+                          (file-name-sans-extension
+                           (file-name-nondirectory f2))
+                          ext1))
+                 t)))
+      (mapc (lambda (f) (rename-file f "..")) provides))
+    (mis-abort)))
 
 ;; ——— Utilities ———————————————————————————————————————————————————————————————
 (defun mis-directory-files (directory)
-  "Return results of `directory-files' without \".\" and \"..\"."
+  "Return results of (`directory-files' DIRECTORY) without \".\" and \"..\"."
   (delete "." (delete ".." (directory-files directory))))
 
 (defun mis-recipes-by-ext (ext)
@@ -240,11 +243,17 @@ In addition to `mis-abort' copy over the files from \"make provide\"."
     (insert str)
     (write-region nil nil file nil 1)))
 
+(defun mis-all-equal (lst)
+  "Return t if all elements of LST are equal."
+  (cl-every (lambda (x) (equal x (car lst)))
+            (cdr lst)))
+
 (defun mis-action (x)
   "Make it so for recipe X."
-  (let* ((source (dired-get-filename nil t))
+  (let* ((source (file-name-nondirectory (car mis-source-files)))
          (ext (file-name-extension source))
-         (basedir (file-name-directory source))
+         (basedir (or (file-name-directory source)
+                      default-directory))
          (dir (expand-file-name
                (format "%s:%s" x (file-name-nondirectory source))
                basedir))
@@ -258,12 +267,29 @@ In addition to `mis-abort' copy over the files from \"make provide\"."
       (if (string-match "make:" requires)
           (error "Makefile must have a \"require\" target")
         (mkdir dir)
-        (mapc (lambda (f) (rename-file f (expand-file-name f dir)))
-              (cons "Makefile" (split-string requires "\n" t)))
-        (rename-file source
-                     (expand-file-name (concat "in." ext) dir))
-        (mis-spit requires (expand-file-name "requires" dir))))
+        (setq mis-source-files
+              (append mis-source-files
+                      (mapcar 'expand-file-name
+                              (split-string requires "\n" t))))
+        (setq mis-target-files
+              (mapcar (lambda (x) (mis-rename-quote x dir))
+                      mis-source-files))
+        (rename-file "Makefile" dir)
+        ;; (mis-spit requires (expand-file-name "requires" dir))
+        ))
     (find-file (expand-file-name "Makefile" dir))))
+
+(defun mis-delete-file (file)
+  "Delete FILE."
+  (move-file-to-trash file))
+
+(defun mis-rename-quote (file dir)
+  "Move FILE to DIR, changing spaces to underscores."
+  (let ((dest (expand-file-name
+               (replace-regexp-in-string
+                " " "_" (file-name-nondirectory file)) dir)))
+    (rename-file file dest)
+    dest))
 
 (provide 'make-it-so)
 
